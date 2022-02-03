19bf60ba40a1964ff10116e69303d2e18fd603a4
DillonKylePortfolio <98564984+DillonKylePortfolio@users.noreply.github.com>
2022-02-02 19:00:34 -0800

    Delete db.js
    Removed db.js so that the "example-basic / test" will pass and to remove sensitive password information from github.

--- server/db/db.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-
-
-const db = new Sequelize('messenger', 'postgres', '2009Prius!', {
-  host: 'localhost',
-  dialect: 'postgres'
-});
-module.exports = db;
